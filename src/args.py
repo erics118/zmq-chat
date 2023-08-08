87902913c5f15ec2f0d9c368e71c1c0fdc1df350
erics118 <52634785+erics118@users.noreply.github.com>
2023-08-08 01:37:46 -0400

    add args

---
+++ src/args.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--primary", action="store_true")
+    group.add_argument("--secondary", action="store_true")
+    parser.add_argument("--code", type=str, required=False)
+
+    args = parser.parse_args()
+
+    return args
--- src/main.py
+++ src/main.py
@@ -1,18 +1,10 @@
-import argparse
 import threading
+from args import parseArgs
 from ip import encodeIp
 
 from shared import makePrimarySocket, makeSecondarySocket, recvMessages, sendMessages
 
-
-parser = argparse.ArgumentParser()
-
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("--primary", action="store_true")
-group.add_argument("--secondary", action="store_true")
-parser.add_argument("--code", type=str, required=False)
-
-args = parser.parse_args()
+args = parseArgs()
 
 if args.primary:
     socket, ip = makePrimarySocket()
@@ -21,6 +13,7 @@ if args.secondary:
     socket, ip = makeSecondarySocket(args.code)
 
 print(f"IP Address: {ip}\nRoom Code: {encodeIp(ip)}\n-------------------")
+
 
 # create threads for listening and sending messages
 threading.Thread(target=recvMessages, args=(socket,)).start()
